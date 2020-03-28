46ed51d1c729a3e55cb99560f9104cd522f71c34
Nikolay Selivanov <rfka1@inbox.ru>
2020-03-28 03:30:44 +0300

    Implement contour detector

--- PointsDetection/Application.py
+++ PointsDetection/Application.py
@@ -2,7 +2,10 @@ import tkinter as tk
 
 import VideoModule as vm
 import numpy as np
+import cv2 as cv
 import MyWidgets as mw
+
+
 
 
 class Application(tk.Frame):
@@ -13,20 +16,93 @@ class Application(tk.Frame):
         self.pack()
         self.__create_widgets()
         self.__initCamera()
+        self.__contourDetector = vm.MaskContoursDetector()
 
     def __initCamera(self):
         self.cam = vm.Camera(self.__receiveCameraData)
 
     def __create_widgets(self):
-        randomArray = np.random.rand(255,255, 3) * 255
-        byteArray = randomArray.astype(np.uint8)
-        self.__streamCanvas = mw.ImageCanvas(width = 900, height = 600)
-        self.__streamCanvas.settleImageData(byteArray)
-        self.__streamCanvas.pack()
+        self.__streamCanvas = mw.ImageCanvas(master=self, width=600, height=400)
+        self.__streamCanvas.pack(side=tk.LEFT)
+        self.__initContourDetectorSettingsWidgets()
+
+    def __initContourDetectorSettingsWidgets(self):
+        self.__colorSettingsControl = tk.Canvas(master=self, width=600, height=600, bg="blue")
+        self.__colorSettingsControl.pack_propagate(0)
+
+        # Lower border
+        self.__lowerMaskLabel = tk.Label(master = self.__colorSettingsControl, text="Lower mask border")
+        self.__lowerMaskLabel.pack(fill=tk.X, side=tk.TOP)
+
+        self.__hueMinSeek = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                     from_=0, to=255, label="Hue", command=self.__changeLowMask)
+        self.__hueMinSeek.set(94)
+        self.__hueMinSeek.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        self.__saturationMinSeek = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                            from_=0, to=255, label="Saturtation", command=self.__changeLowMask)
+        self.__saturationMinSeek.set(43)
+        self.__saturationMinSeek.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        self.__valueMinSeek = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                       from_=0, to=255, label="Value", command=self.__changeLowMask)
+        self.__valueMinSeek.set(23)
+        self.__valueMinSeek.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        # Delimiter
+        self.__maskSettingsDelimiter = tk.Frame(master = self.__colorSettingsControl, height=20)
+        self.__maskSettingsDelimiter.pack(fill=tk.X, side=tk.TOP)
+
+        # Upper border
+        self.__upperMaskLabel = tk.Label(master = self.__colorSettingsControl, text="Upper mask border")
+        self.__upperMaskLabel.pack(fill=tk.X, side=tk.TOP)
+
+        self.__hueMaxSeek = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                     from_=0, to=255, label="Hue", command=self.__changeUpMask)
+        self.__hueMaxSeek.set(153)
+        self.__hueMaxSeek.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        self.__saturationMaxSeek = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                            from_=0, to=255, label="Saturtation", command=self.__changeUpMask)
+        self.__saturationMaxSeek.set(255)
+        self.__saturationMaxSeek.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        self.__valueMaxSeek = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                       from_=0, to=255, label="Value", command=self.__changeUpMask)
+        self.__valueMaxSeek.set(255)
+        self.__valueMaxSeek.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        # Delimiter
+        self.__maskSettingsDelimiter2 = tk.Frame(master = self.__colorSettingsControl, height=20)
+        self.__maskSettingsDelimiter2.pack(fill=tk.X, side=tk.TOP)
+
+        # Contour area
+        self.__minContourAreaLabel = tk.Label(master = self.__colorSettingsControl, text="Contour area")
+        self.__minContourAreaLabel.pack(fill=tk.X, side=tk.TOP)
+
+        self.__contourAreaMin = tk.Scale(master=self.__colorSettingsControl, orient=tk.HORIZONTAL, bg="red",
+                                     from_=0, to=1000, label="Min contour area", command=self.__changeContourAreaMin)
+        self.__contourAreaMin.set(200)
+        self.__contourAreaMin.pack(fill=tk.X, side=tk.TOP, pady=(5,5), padx=20)
+
+        self.__colorSettingsControl.pack(side=tk.TOP)
+
+    def __changeLowMask(self, _):
+        lowerMaskBorder = (self.__hueMinSeek.get(), self.__saturationMinSeek.get(), self.__valueMinSeek.get())
+        self.__contourDetector.setLowerMaskBorder(lowerMaskBorder)
+
+    def __changeUpMask(self, _):
+        upperMaskBorder = (self.__hueMaxSeek.get(), self.__saturationMaxSeek.get(), self.__valueMaxSeek.get())
+        self.__contourDetector.setUpperMaskBorder(upperMaskBorder)
+
+    def __changeContourAreaMin(self, value):
+        self.__contourDetector.setContourAreaMin(int(value))
 
     def __receiveCameraData(self, data):
+        data = cv.cvtColor(data, cv.COLOR_BGR2HSV)
+        data, contours = self.__contourDetector.findContours(data)
+        data = cv.cvtColor(data, cv.COLOR_HSV2RGB)
         self.__streamCanvas.settleImageData(data)
 
     def dispose(self):
         self.cam.dispose()
-
--- PointsDetection/videoModule.py
+++ PointsDetection/videoModule.py
@@ -1,5 +1,5 @@
 import threading
-
+import numpy as np
 import cv2
 
 
@@ -26,14 +26,44 @@ class Camera():
             if self.__isStopped():
                 return
             ret, frame = self.__cap.read()
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             if ret and self.__dataReceiveCallback != None:
                 try:
                     self.__dataReceiveCallback(frame)
                 except Exception as inst:
                     print("error")
 
-    
     def dispose(self):
         self.__stop()
         self.__cap.release()
+
+class MaskContoursDetector():
+
+    def __init__(self, lowerMaskBorder = (0, 0, 0), upperMaskBorder = (255, 255, 255), contourAreaMin = 0):
+        self.__lowerMaskBorder = lowerMaskBorder
+        self.__upperMaskBoreder = upperMaskBorder
+        self.__contourAreaMin = contourAreaMin
+
+    def getLowerMaskBorder(self):
+        return self.__lowerMaskBorder
+
+    def getUpperMaskBorder(self):
+        return self.__upperMaskBoreder
+
+    def setLowerMaskBorder(self, hsvTuple):
+        self.__lowerMaskBorder = hsvTuple
+
+    def setUpperMaskBorder(self, hsvTuple):
+        self.__upperMaskBoreder = hsvTuple
+
+    def setContourAreaMin(self, value):
+        self.__contourAreaMin = value
+
+    def getcontourAreaMin(self):
+        return self.__contourAreaMin
+
+    def findContours(self, hsvData):
+        mask = cv2.inRange(hsvData, self.__lowerMaskBorder, self.__upperMaskBoreder)
+        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = [c for c in contours if cv2.contourArea(c)>self.__contourAreaMin]
+        frame = cv2.drawContours(hsvData, contours, -1, (0,255,255), thickness=5)
+        return frame, contours
